8a007148ef3a6c4933018a9b9ff71a45aa590beb
sajou1 <sergebana006@gmail.com>
2023-07-20 12:23:25 -0700

    101-natural.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,7 +12,7 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-	for (num = 0; num <= n; num++)
+	for (num = 0; num <= n; mult++)
 	{
 	_putchar('0');
 	for (mult = 1; mult <= n; mult++)
@@ -27,15 +27,5 @@ void print_times_table(int n)
 	if (prod >= 100)
 	{
 	_putchar((prod / 10) + '0');
-	_putchar((prod / 10) % 10 + '0');
-	}
-	else if (prod <= 99 && prod >= 10)
-	{
-	_putchar((prod / 10) + '0');
-	}
-	_putchar((prod % 10) + '0');
-	}
-	_putchar('\n');
-	}
-	}
+	_putchar((prod / 10) + 
 }
